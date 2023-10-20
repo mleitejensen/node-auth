11148f95ef9399b22947d7c9f683838e8f5abb15
Martin <marekatt2004@gmail.com>
2023-10-20 11:18:01 +0200

    put the dbURI in another loacl file

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/authRoutes")
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware")
+const { dbURI } = require("./dbURI.js")
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection 
-const dbURI = 'mongodb+srv://netninja:Passord1@node.phrgrjy.mongodb.net/node-auth?retryWrites=true&w=majority&appName=AtlasApp';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const db = dbURI
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
+app.get("*", checkUser )
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
